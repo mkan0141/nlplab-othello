997ca3662e335ddee22ffc3bb237fdea526bba24
iz470 <44053293+iz470@users.noreply.github.com>
2018-10-16 22:44:53 +0900

    Update board.py

--- board.py
+++ board.py
@@ -10,11 +10,27 @@ def init():
     board[4][3] = board[3][4] = BLACK
     return board
 
-
-
-
-
-
+# 盤面を表示する関数です.
+def show_board(board):
+    print("  A B C D E F G H")
+    x = -1
+    y = 0
+    while y < 7:
+        while x < 8:
+            if x == -1:
+                print(y+1, end="")
+            else:
+                if board[x][y] == NONE:
+                    print(" .", end="")
+                elif board[x][y] == WHITE:
+                    print(" o", end="") 
+                elif board[x][y] == BLACK:
+                    print(" x", end="")               
+            x += 1
+        x = -1
+        y += 1
+        print()
+        
 """
 下のmain関数は
 デバッグなどに使ってください.
