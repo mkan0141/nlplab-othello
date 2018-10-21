b32af72b8d7de8a3167f51b4f121f43874142028
SYUNYA <b16069>
2018-10-22 00:13:18 +0900

    勝ち負け判定プログラム

--- decision.py
+++ decision.py
@@ -10,11 +10,11 @@ def decision(board):
 
     while y < 8:
         while x < 8:
-        if board[x][y] == WHITE:
-            w += 1
+            if board[x][y] == WHITE:
+                w += 1
             elif board[x][y] == BLACK:
                 b += 1              
-                x += 1
+            x += 1
         x = 0
         y += 1
     
@@ -22,5 +22,5 @@ def decision(board):
         return WHITE
     elif w < b:
         return BLACK
-    elif W == b:
+    elif w == b:
         return NONE
