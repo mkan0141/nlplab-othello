49bd4ba40098a524b024fb6770cde87cd3a62bee
ikawa06 <44053111+ikawa06@users.noreply.github.com>
2018-10-22 02:09:01 +0900

    player.py_ver1.01
    入力が不正な場合，エラー文を吐いて再度入力させるようになった．
    正しい入力が得られるまで無限に繰り返す．

--- player.py
+++ player.py
@@ -1,49 +1,66 @@
 
 def Put_koma():
+    
 
 
-    #入力処理
-    print("駒をどこに置くか指定してください．(A1 ~ H8)")
-    place = input()
+    while True :
+        
+        Flag = 0
+        Flag2 = 0
+        
+
+        #入力処理
+        print("駒をどこに置くか指定してください．(A1 ~ H8)")
+        place = input()
     
-    tate = int( place[1:2] )
-    yoko = place[0:1]
-    
-    
-    
-    #縦の処理
-    if 8 < tate or tate < 1:
-        print("2桁目の数値は 1 ~ 8 の数値で入力してください．")
-        tate = NONE
-        yoko = NONE
-    else:
-        tate = tate - 1
+        tate = int( place[1:2] )
+        yoko = place[0:1]
     
     
-    #横の処理
-    if yoko == "A" or yoko == "a":
-        yoko = 0
-    elif yoko == "B" or yoko == "b":
-        yoko = 1
-    elif yoko == "C" or yoko == "c":
-        yoko = 2
-    elif yoko == "D" or yoko == "d":
-        yoko = 3
-    elif yoko == "E" or yoko == "e":
-        yoko = 4
-    elif yoko == "F" or yoko == "f":
-        yoko = 5
-    elif yoko == "G" or yoko == "g":
-        yoko = 6
-    elif yoko == "H" or yoko == "h":
-        yoko = 7
-    else :
-        print("1桁目の入力は A ~ H　を入力してください．")
-        tate = NONE
-        yoko = NONE
+        #縦の処理
+        if 8 < tate or tate < 1:
+            print("不正な入力値です：2桁目の数値は 1 ~ 8 の数値で入力してください．")
+        else:
+            tate = tate - 1
+            Flag = 1
+    
+    
+        #横の処理
+        if yoko == "A" or yoko == "a":
+            yoko = 0
+            Flag2 = 1
+        elif yoko == "B" or yoko == "b":
+            yoko = 1
+            Flag2 = 1
+        elif yoko == "C" or yoko == "c":
+            yoko = 2
+            Flag2 = 1
+        elif yoko == "D" or yoko == "d":
+            yoko = 3
+            Flag2 = 1
+        elif yoko == "E" or yoko == "e":
+            yoko = 4
+            Flag2 = 1
+        elif yoko == "F" or yoko == "f":
+            yoko = 5
+            Flag2 = 1
+        elif yoko == "G" or yoko == "g":
+            yoko = 6
+            Flag2 = 1
+        elif yoko == "H" or yoko == "h":
+            yoko = 7
+            Flag2 = 1
+        else :
+            print("不正な入力値です：1桁目の入力は A ~ H　を入力してください．")
 
 
-    #戻り値(NONEをintでキャストした場合はどうなるか知らんｗ)
+
+    
+        if Flag == 1 and Flag2 == 1:
+            break
+
+
+    #戻り値
     return int(tate), int(yoko)
 
 
