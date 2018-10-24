e94d1d8bacc96b6dc133594007179e4a09dcab93
mkan0141 <kanzume0141@gmail.com>
2018-10-24 20:32:51 +0900

    update: クラスにした
    Put_komaをput_stoneへ名前を変更したのと，board.pyとplayer.pyの関数をクラスにした

--- board.py
+++ board.py
@@ -3,335 +3,339 @@ import numpy as np
 NONE  = 0
 BLACK = 1
 WHITE = 2
-
-def init():
-    board = [[NONE for i in range(8)]for j in range(8)]
-    board[3][3] = board[4][4] = WHITE
-    board[4][3] = board[3][4] = BLACK
-    return board
-
-# 盤面を表示する関数です.
-def show_board(board):
-    print("  A B C D E F G H")
-    x = -1
-    y = 0
-    while y < 8:
-        while x < 8:
-            if x == -1:
-                print(y+1, end="")
-            else:
-                if board[x][y] == NONE:
-                    print(" .", end="")
-                elif board[x][y] == WHITE:
-                    print(" o", end="")
-                elif board[x][y] == BLACK:
-                    print(" x", end="")
-            x += 1
-        x = -1
-        y += 1
-        print()
-
-# 石をひっくり返す処理
-
-def reverse(color, x, y, board):
-
-    if color == WHITE:
-        board[x][y] = WHITE
-
-        y1 = y - 1
-        while y1 + 1 > 0:
-            if board[x][y1] == NONE:
-                y1 = -1
-            elif board[x][y1] == WHITE:
-                y2 = y1 + 1
-                y1 = -1
-                while y2 < y:
-                    board[x][y2] = WHITE
-                    y2 += 1
-            y1 -= 1
-        y1 = y + 1
-        while y1 - 1 < 7:
-            if board[x][y1] == NONE:
-                y1 = 7
-            elif board[x][y1] == WHITE:
-                y2 = y1 - 1
-                y1 = 7
-                while y2 > y:
-                    board[x][y2] = WHITE
-                    y2 -= 1
-            y1 += 1
-
-        x1 = x - 1
-        while x1 + 1 > 0:
-            if board[x1][y] == NONE:
-                x1 = -1
-            elif board[x1][y] == WHITE:
-                x2 = x1 + 1
-                x1 = -1
-                while x2 < x:
-                    board[x2][y] = WHITE
-                    x2 += 1
-            x1 -= 1
-        x1 = x + 1
-        while x1 - 1 < 7:
-            if board[x1][y] == NONE:
-                x1 = 7
-            elif board[x1][y] == WHITE:
-                x2 = x1 - 1
-                x1 = 7
-                while x2 > x:
-                    board[x2][y] = WHITE
-                    x2 -= 1
-            x1 += 1
-
-        x1 = x - 1
-        y1 = y - 1
-        while x1 + 1 > 0 and y1 + 1 > 0:
-            if board[x1][y1] == NONE:
-                x1 = -1
-            elif board[x1][y1] == WHITE:
-                x2 = x1 + 1
-                y2 = y1 + 1
-                x1 = -1
-                while x2 < x:
-                    board[x2][y2] = WHITE
-                    x2 += 1
-                    y2 += 1
-            x1 -= 1
-            y1 -= 1
-        x1 = x + 1
-        y1 = y + 1
-        while x1 - 1 < 7 and y1 - 1 < 7:
-            if board[x1][y1] == NONE:
-                x1 = 7
-            elif board[x1][y1] == WHITE:
-                x2 = x1 - 1
-                y2 = y1 - 1
-                x1 = 7
-                while x2 > x:
-                    board[x2][y2] = WHITE
-                    x2 -= 1
-                    y2 -= 1
-            x1 += 1
-            y1 += 1
-
-        x1 = x - 1
-        y1 = y + 1
-        while x1 + 1 > 0 and y1 - 1 < 7:
-            if board[x1][y1] == NONE:
-                x1 = -1
-            elif board[x1][y1] == WHITE:
-                x2 = x1 + 1
-                y2 = y1 - 1
-                x1 = -1
-                while x2 < x:
-                    board[x2][y2] = WHITE
-                    x2 += 1
-                    y2 -= 1
-            x1 -= 1
-            y1 += 1
-        x1 = x + 1
-        y1 = y - 1
-        while x1 - 1 < 7 and y1 + 1 > 0:
-            if board[x1][y1] == NONE:
-                x1 = 7
-            elif board[x1][y1] == WHITE:
-                x2 = x1 - 1
-                y2 = y1 + 1
-                x1 = 7
-                while x2 > x:
-                    board[x2][y2] = WHITE
-                    x2 -= 1
-                    y2 += 1
-            x1 += 1
-            y1 -= 1
-
-
-    elif color == BLACK:
-        board[x][y] = BLACK
-
-        y1 = y - 1
-        while y1 + 1 > 0:
-            if board[x][y1] == NONE:
-                y1 = -1
-            elif board[x][y1] == BLACK:
-                y2 = y1 + 1
-                y1 = -1
-                while y2 < y:
-                    board[x][y2] = BLACK
-                    y2 += 1
-            y1 -= 1
-        y1 = y + 1
-        while y1 - 1 < 7:
-            if board[x][y1] == NONE:
-                y1 = 7
-            elif board[x][y1] == BLACK:
-                y2 = y1 - 1
-                y1 = 7
-                while y2 > y:
-                    board[x][y2] = BLACK
-                    y2 -= 1
-            y1 += 1
-
-        x1 = x - 1
-        while x1 + 1 > 0:
-            if board[x1][y] == NONE:
-                x1 = -1
-            elif board[x1][y] == BLACK:
-                x2 = x1 + 1
-                x1 = -1
-                while x2 < x:
-                    board[x2][y] = BLACK
-                    x2 += 1
-            x1 -= 1
-        x1 = x + 1
-        while x1 - 1 < 7:
-            if board[x1][y] == NONE:
-                x1 = 7
-            elif board[x1][y] == BLACK:
-                x2 = x1 - 1
-                x1 = 7
-                while x2 > x:
-                    board[x2][y] = BLACK
-                    x2 -= 1
-            x1 += 1
-
-        x1 = x - 1
-        y1 = y - 1
-        while x1 + 1 > 0 and y1 + 1 > 0:
-            if board[x1][y1] == NONE:
-                x1 = -1
-            elif board[x1][y1] == BLACK:
-                x2 = x1 + 1
-                y2 = y1 + 1
-                x1 = -1
-                while x2 < x:
-                    board[x2][y2] = BLACK
-                    x2 += 1
-                    y2 += 1
-            x1 -= 1
-            y1 -= 1
-        x1 = x + 1
-        y1 = y + 1
-        while x1 - 1 < 7 and y1 - 1 < 7:
-            if board[x1][y1] == NONE:
-                x1 = 7
-            elif board[x1][y1] == BLACK:
-                x2 = x1 - 1
-                y2 = y1 - 1
-                x1 = 7
-                while x2 > x:
-                    board[x2][y2] = BLACK
-                    x2 -= 1
-                    y2 -= 1
-            x1 += 1
-            y1 += 1
-
-        x1 = x - 1
-        y1 = y + 1
-        while x1 + 1 > 0 and y1 - 1 < 7:
-            if board[x1][y1] == NONE:
-                x1 = -1
-            elif board[x1][y1] == BLACK:
-                x2 = x1 + 1
-                y2 = y1 - 1
-                x1 = -1
-                while x2 < x:
-                    board[x2][y2] = BLACK
-                    x2 += 1
-                    y2 -= 1
-            x1 -= 1
-            y1 += 1
-        x1 = x + 1
-        y1 = y - 1
-        while x1 - 1 < 7 and y1 + 1 > 0:
-            if board[x1][y1] == NONE:
-                x1 = 7
-            elif board[x1][y1] == BLACK:
-                x2 = x1 - 1
-                y2 = y1 + 1
-                x1 = 7
-                while x2 > x:
-                    board[x2][y2] = BLACK
-                    x2 -= 1
-                    y2 += 1
-            x1 += 1
-            y1 -= 1
-
-    return board
-
-
-#置ける場所リスト
 dx = [0,1,1,1,0,-1,-1,-1]
 dy = [1,1,0,-1,-1,-1,0,1]
 
-def ayasi(x,y,board,color):
-    aitecolor = NONE
-    if color == BLACK:
-        aitecolor = WHITE
-    else:
-        aitecolor = BLACK
-    for i in range(8):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if nx >=0 and nx <8 and ny >= 0 and ny <8 and board[ny][nx] == aitecolor:
-            return True
-    return False
+class Board():
+    def __init__(self):
+        self.board = [[NONE for i in range(8)]for j in range(8)]
 
-def check(x,y,board,color):
-    aitecolor = NONE
-    if color == BLACK:
-        aitecolor = WHITE
-    else:
-        aitecolor = BLACK
-    for i in range(8):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if nx >=0 and nx <8 and ny >= 0 and ny <8 and board[ny][nx] == aitecolor:
-            while True:
-                nx += dx[i]
-                ny += dy[i]
-                if nx >=0 and nx <8 and ny >= 0 and ny <8 and board[ny][nx] == aitecolor:
-                    continue
-                elif nx >=0 and nx <8 and ny >= 0 and ny <8 and board[ny][nx] == color:
-                    return True
+
+    def init(self):
+        self.board = [[NONE for i in range(8)]for j in range(8)]
+        self.board[3][3] = self.board[4][4] = WHITE
+        self.board[4][3] = self.board[3][4] = BLACK
+
+    # 盤面を表示する関数です.
+    def show_board(self):
+        print("  A B C D E F G H")
+        x = -1
+        y = 0
+        while y < 8:
+            while x < 8:
+                if x == -1:
+                    print(y+1, end="")
                 else:
-                    break
-    return False
+                    if self.board[x][y] == NONE:
+                        print(" .", end="")
+                    elif self.board[x][y] == WHITE:
+                        print(" o", end="")
+                    elif self.board[x][y] == BLACK:
+                        print(" x", end="")
+                x += 1
+            x = -1
+            y += 1
+            print()
+
+    # 石をひっくり返す処理
+
+    def reverse(self, color, x, y):
+
+        if color == WHITE:
+            self.board[x][y] = WHITE
+
+            y1 = y - 1
+            while y1 + 1 > 0:
+                if self.board[x][y1] == NONE:
+                    iy1 = -1
+                elif self.board[x][y1] == WHITE:
+                    y2 = y1 + 1
+                    y1 = -1
+                    while y2 < y:
+                        self.board[x][y2] = WHITE
+                        y2 += 1
+                y1 -= 1
+            y1 = y + 1
+            while y1 - 1 < 7:
+                if self.board[x][y1] == NONE:
+                    y1 = 7
+                elif self.board[x][y1] == WHITE:
+                    y2 = y1 - 1
+                    y1 = 7
+                    while y2 > y:
+                        self.board[x][y2] = WHITE
+                        y2 -= 1
+                y1 += 1
+
+            x1 = x - 1
+            while x1 + 1 > 0:
+                if self.board[x1][y] == NONE:
+                    x1 = -1
+                elif self.board[x1][y] == WHITE:
+                    x2 = x1 + 1
+                    x1 = -1
+                    while x2 < x:
+                        self.board[x2][y] = WHITE
+                        x2 += 1
+                x1 -= 1
+            x1 = x + 1
+            while x1 - 1 < 7:
+                if self.board[x1][y] == NONE:
+                    x1 = 7
+                elif self.board[x1][y] == WHITE:
+                    x2 = x1 - 1
+                    x1 = 7
+                    while x2 > x:
+                        self.board[x2][y] = WHITE
+                        x2 -= 1
+                x1 += 1
+
+            x1 = x - 1
+            y1 = y - 1
+            while x1 + 1 > 0 and y1 + 1 > 0:
+                if self.board[x1][y1] == NONE:
+                    x1 = -1
+                elif self.board[x1][y1] == WHITE:
+                    x2 = x1 + 1
+                    y2 = y1 + 1
+                    x1 = -1
+                    while x2 < x:
+                        self.board[x2][y2] = WHITE
+                        x2 += 1
+                        y2 += 1
+                x1 -= 1
+                y1 -= 1
+            x1 = x + 1
+            y1 = y + 1
+            while x1 - 1 < 7 and y1 - 1 < 7:
+                if self.board[x1][y1] == NONE:
+                    x1 = 7
+                elif self.board[x1][y1] == WHITE:
+                    x2 = x1 - 1
+                    y2 = y1 - 1
+                    x1 = 7
+                    while x2 > x:
+                        self.board[x2][y2] = WHITE
+                        x2 -= 1
+                        y2 -= 1
+                x1 += 1
+                y1 += 1
+
+            x1 = x - 1
+            y1 = y + 1
+            while x1 + 1 > 0 and y1 - 1 < 7:
+                if self.board[x1][y1] == NONE:
+                    x1 = -1
+                elif self.board[x1][y1] == WHITE:
+                    x2 = x1 + 1
+                    y2 = y1 - 1
+                    x1 = -1
+                    while x2 < x:
+                        self.board[x2][y2] = WHITE
+                        x2 += 1
+                        y2 -= 1
+                x1 -= 1
+                y1 += 1
+            x1 = x + 1
+            y1 = y - 1
+            while x1 - 1 < 7 and y1 + 1 > 0:
+                if self.board[x1][y1] == NONE:
+                    x1 = 7
+                elif self.board[x1][y1] == WHITE:
+                    x2 = x1 - 1
+                    y2 = y1 + 1
+                    x1 = 7
+                    while x2 > x:
+                        self.board[x2][y2] = WHITE
+                        x2 -= 1
+                        y2 += 1
+                x1 += 1
+                y1 -= 1
 
 
-def get_valid_position(color, board):
-    list = []
-    for x in range(8):
-        for y in range(8):
-            if ayasi(x,y,board,color) == True:
-                if check(x,y,board,color) == True:
-                    list.append([x,y])
-    return list
+        elif color == BLACK:
+            self.board[x][y] = BLACK
+
+            y1 = y - 1
+            while y1 + 1 > 0:
+                if self.board[x][y1] == NONE:
+                    y1 = -1
+                elif self.board[x][y1] == BLACK:
+                    y2 = y1 + 1
+                    y1 = -1
+                    while y2 < y:
+                        self.board[x][y2] = BLACK
+                        y2 += 1
+                y1 -= 1
+            y1 = y + 1
+            while y1 - 1 < 7:
+                if self.board[x][y1] == NONE:
+                    y1 = 7
+                elif self.board[x][y1] == BLACK:
+                    y2 = y1 - 1
+                    y1 = 7
+                    while y2 > y:
+                        self.board[x][y2] = BLACK
+                        y2 -= 1
+                y1 += 1
+
+            x1 = x - 1
+            while x1 + 1 > 0:
+                if self.board[x1][y] == NONE:
+                    x1 = -1
+                elif self.board[x1][y] == BLACK:
+                    x2 = x1 + 1
+                    x1 = -1
+
+                    while x2 < x:
+                        self.board[x2][y] = BLACK
+                        x2 += 1
+                x1 -= 1
+            x1 = x + 1
+            while x1 - 1 < 7:
+                if self.board[x1][y] == NONE:
+                    x1 = 7
+                elif self.board[x1][y] == BLACK:
+                    x2 = x1 - 1
+                    x1 = 7
+                    while x2 > x:
+                        self.board[x2][y] = BLACK
+                        x2 -= 1
+                x1 += 1
+
+            x1 = x - 1
+            y1 = y - 1
+            while x1 + 1 > 0 and y1 + 1 > 0:
+                if self.board[x1][y1] == NONE:
+                    x1 = -1
+                elif self.board[x1][y1] == BLACK:
+                    x2 = x1 + 1
+                    y2 = y1 + 1
+                    x1 = -1
+                    while x2 < x:
+                        self.board[x2][y2] = BLACK
+                        x2 += 1
+                        y2 += 1
+                x1 -= 1
+                y1 -= 1
+            x1 = x + 1
+            y1 = y + 1
+            while x1 - 1 < 7 and y1 - 1 < 7:
+                if self.board[x1][y1] == NONE:
+                    x1 = 7
+                elif self.board[x1][y1] == BLACK:
+                    x2 = x1 - 1
+                    y2 = y1 - 1
+                    x1 = 7
+                    while x2 > x:
+                        self.board[x2][y2] = BLACK
+                        x2 -= 1
+                        y2 -= 1
+                x1 += 1
+                y1 += 1
+
+            x1 = x - 1
+            y1 = y + 1
+            while x1 + 1 > 0 and y1 - 1 < 7:
+                if self.board[x1][y1] == NONE:
+                    x1 = -1
+                elif self.board[x1][y1] == BLACK:
+                    x2 = x1 + 1
+                    y2 = y1 - 1
+                    x1 = -1
+                    while x2 < x:
+                        self.board[x2][y2] = BLACK
+                        x2 += 1
+                        y2 -= 1
+                x1 -= 1
+                y1 += 1
+            x1 = x + 1
+            y1 = y - 1
+            while x1 - 1 < 7 and y1 + 1 > 0:
+                if self.board[x1][y1] == NONE:
+                    x1 = 7
+                elif self.board[x1][y1] == BLACK:
+                    x2 = x1 - 1
+                    y2 = y1 + 1
+                    x1 = 7
+                    while x2 > x:
+                        self.board[x2][y2] = BLACK
+                        x2 -= 1
+                        y2 += 1
+                x1 += 1
+                y1 -= 1
 
 
-def who_won(board):
-    x = 0
-    y = 0
-    w = 0
-    b = 0
 
-    while y < 8:
-        while x < 8:
-            if board[x][y] == WHITE:
-                w += 1
-            elif board[x][y] == BLACK:
-                b += 1
-            x += 1
+
+    #置ける場所リスト
+    def ayasi(self, x, y, color):
+        aitecolor = NONE
+        if color == BLACK:
+            aitecolor = WHITE
+        else:
+            aitecolor = BLACK
+        for i in range(8):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx >=0 and nx <8 and ny >= 0 and ny <8 and self.board[ny][nx] == aitecolor:
+                return True
+        return False
+
+    def check(self, x, y, color):
+        aitecolor = NONE
+        if color == BLACK:
+            aitecolor = WHITE
+        else:
+            aitecolor = BLACK
+        for i in range(8):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx >=0 and nx <8 and ny >= 0 and ny <8 and self.board[ny][nx] == aitecolor:
+                while True:
+                    nx += dx[i]
+                    ny += dy[i]
+                    if nx >=0 and nx <8 and ny >= 0 and ny <8 and self.board[ny][nx] == aitecolor:
+                        continue
+                    elif nx >=0 and nx <8 and ny >= 0 and ny <8 and self.board[ny][nx] == color:
+                        return True
+                    else:
+                        break
+        return False
+
+
+    def get_valid_position(self, color):
+        list = []
+        for x in range(8):
+            for y in range(8):
+                if self.ayasi(x, y, color) == True:
+                    if self.check(x, y, color) == True:
+                        list.append([x,y])
+        return list
+
+
+    def who_won(self):
         x = 0
-        y += 1
+        y = 0
+        w = 0
+        b = 0
 
-    if w > b:
-        return WHITE
-    elif w < b:
-        return BLACK
-    elif w == b:
-        return NONE
+        while y < 8:
+            while x < 8:
+                if self.board[x][y] == WHITE:
+                    w += 1
+                elif self.board[x][y] == BLACK:
+                    b += 1
+                x += 1
+            x = 0
+            y += 1
+
+        if w > b:
+            return WHITE
+        elif w < b:
+            return BLACK
+        elif w == b:
+            return NONE
 
 
 """
@@ -339,9 +343,18 @@ def who_won(board):
 デバッグなどに使ってください.
 """
 def main():
-    board = init()
-    okeru = get_valid_position(BLACK, board)
-    print(okeru)
+    board = Board()
+    color = WHITE
+    while True:
+        if color == WHITE:
+            colot = BLACK
+        else:
+            color = WHITE
+
+        list = board.get_valid_position(color)
+        board.show_board()
+        print(list)
+
 
 
 # これより下はいじらない
--- player.py
+++ player.py
@@ -1,68 +1,58 @@
+class Player():
+    def put_stone(self):
+        while True :
 
-def Put_koma():
-    
-
-
-    while True :
-        
-        Flag = 0
-        Flag2 = 0
-        
-
-        #入力処理
-        print("駒をどこに置くか指定してください．(A1 ~ H8)")
-        place = input()
-    
-        tate = int( place[1:2] )
-        yoko = place[0:1]
-    
-    
-        #縦の処理
-        if 8 < tate or tate < 1:
-            print("不正な入力値です：2桁目の数値は 1 ~ 8 の数値で入力してください．")
-        else:
-            tate = tate - 1
-            Flag = 1
-    
-    
-        #横の処理
-        if yoko == "A" or yoko == "a":
-            yoko = 0
-            Flag2 = 1
-        elif yoko == "B" or yoko == "b":
-            yoko = 1
-            Flag2 = 1
-        elif yoko == "C" or yoko == "c":
-            yoko = 2
-            Flag2 = 1
-        elif yoko == "D" or yoko == "d":
-            yoko = 3
-            Flag2 = 1
-        elif yoko == "E" or yoko == "e":
-            yoko = 4
-            Flag2 = 1
-        elif yoko == "F" or yoko == "f":
-            yoko = 5
-            Flag2 = 1
-        elif yoko == "G" or yoko == "g":
-            yoko = 6
-            Flag2 = 1
-        elif yoko == "H" or yoko == "h":
-            yoko = 7
-            Flag2 = 1
-        else :
-            print("不正な入力値です：1桁目の入力は A ~ H　を入力してください．")
+            Flag = 0
+            Flag2 = 0
 
 
+            #入力処理
+            print("駒をどこに置くか指定してください．(A1 ~ H8)")
+            place = input()
 
-    
-        if Flag == 1 and Flag2 == 1:
-            break
+            tate = int( place[1:2] )
+            yoko = place[0:1]
+
+            #縦の処理
+            if 8 < tate or tate < 1:
+                print("不正な入力値です：2桁目の数値は 1 ~ 8 の数値で入力してください．")
+            else:
+                tate = tate - 1
+                Flag = 1
 
 
-    #戻り値
-    return int(tate), int(yoko)
+            #横の処理
+            if yoko == "A" or yoko == "a":
+                yoko = 0
+                Flag2 = 1
+            elif yoko == "B" or yoko == "b":
+                yoko = 1
+                Flag2 = 1
+            elif yoko == "C" or yoko == "c":
+                yoko = 2
+                Flag2 = 1
+            elif yoko == "D" or yoko == "d":
+                yoko = 3
+                Flag2 = 1
+            elif yoko == "E" or yoko == "e":
+                yoko = 4
+                Flag2 = 1
+            elif yoko == "F" or yoko == "f":
+                yoko = 5
+                Flag2 = 1
+            elif yoko == "G" or yoko == "g":
+                yoko = 6
+                Flag2 = 1
+            elif yoko == "H" or yoko == "h":
+                yoko = 7
+                Flag2 = 1
+            else :
+                print("不正な入力値です：1桁目の入力は A ~ H　を入力してください．")
 
 
+            if Flag == 1 and Flag2 == 1:
+                break
 
+        #戻り値
+        return int(tate), int(yoko)
 
