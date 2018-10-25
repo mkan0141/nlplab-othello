5ca3313105b946811a4558e0130ed3bcdd024943
mkan0141 <kanzume0141@gmail.com>
2018-10-26 00:21:43 +0900

    update: random AIをクラスにした

--- AI.py
+++ AI.py
@@ -1,3 +1,9 @@
+import random
 
-def random_ai():
-    # この行を消して書き始めてください
+class RandomAI:
+    def random_ai(self, list):
+        n = random.randrange(len(list))
+        x = list[n][0]
+        y = list[n][1]
+        return x,y
+
